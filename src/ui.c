1fbb510ccd6eff323e827dd73853a935126c3f60
Alexander Panko <god@panki.ru>
2019-12-20 18:59:14 +0100

    Сloses #2: Simplify windows manipulations

--- src/ui.c
+++ src/ui.c
@@ -2,6 +2,15 @@
 #include "game.h"
 #include "ui.h"
 
+int utf8len(char *s)
+{
+    int len = 0;
+    for (; *s; ++s)
+        if ((*s & 0xC0) != 0x80)
+            ++len;
+    return len;
+}
+
 void init_screen()
 {
     setlocale(LC_ALL, ""); // Инициализация локали (обязательна)
@@ -13,10 +22,27 @@ void init_screen()
     refresh();
 }
 
+WINDOW *create_window(char width, char height, char *title)
+{
+    char x = (getmaxx(stdscr) - width) / 2;
+    char y = (getmaxy(stdscr) - height) / 2;
+    WINDOW *w = newwin(height, width, y, x);
+    box(w, 0, 0);
+    mvwprintw(w, 0, (width - utf8len(title) - 4) / 2, "┤ %s ├", title);
+    wrefresh(w);
+    keypad(w, TRUE);
+    return w;
+}
+
+void close_window(WINDOW *w)
+{
+    wclear(w);   // Очищаем окно
+    wrefresh(w); // Обновляем экран
+    delwin(w);   // Удаляем окно
+}
+
 MENU_ITEM main_menu(MENU_ITEM default_item)
 {
-    WINDOW *w;
-
     char current_item = default_item >= 0 && default_item < MENU_ITEMS_COUNT ? default_item : 0;
 
     char *menu_items[MENU_ITEMS_COUNT];
@@ -24,15 +50,7 @@ MENU_ITEM main_menu(MENU_ITEM default_item)
     menu_items[ITEM_HELP] = "Помощь    ";
     menu_items[ITEM_EXIT] = "Выход     ";
 
-    char width = 20;                              // Ширина окна
-    char height = 7;                              // Высота окна
-    char x = (getmaxx(stdscr) - width) / 2;       // По центру
-    char y = (getmaxy(stdscr) - height) / 2;      // По центру
-    w = newwin(height, width, y, x);              // Новое окно
-    box(w, 0, 0);                                 // Рамка
-    mvwprintw(w, 0, (width - 8) / 2, "┤ Меню ├"); // Заголовок
-    wrefresh(w);                                  // отобразить окно
-    keypad(w, TRUE);                              // разрешить ввод с клавиатуры для окна
+    WINDOW *w = create_window(20, 7, "Меню");
 
     while (true)
     {
@@ -52,10 +70,8 @@ MENU_ITEM main_menu(MENU_ITEM default_item)
         case KEY_DOWN:
             current_item < MENU_ITEMS_COUNT - 1 && current_item++;
             break;
-        case 10:         // KEY_ENTER
-            wclear(w);   // Очищаем окно
-            wrefresh(w); // Обновляем экран
-            delwin(w);   // Удаляем окно
+        case 10: // KEY_ENTER
+            close_window(w);
             return current_item;
         }
     }
@@ -71,18 +87,13 @@ screen_state new_game_screen()
     char ch_space = 0; // расстояние между клетками по вертикали
     char cw_space = 1; // расстояние между клетками по горищонтали
 
-    char framew = cw * BOARD_SIZE + cw_space * (BOARD_SIZE + 1) + 2; // ширина рамки
-    char frameh = ch * BOARD_SIZE + ch_space * (BOARD_SIZE + 1) + 2; // высота рамки
-
-    char fx = (getmaxx(stdscr) - framew) / 2; // по центру
-    char fy = (getmaxy(stdscr) - frameh) / 2; // по центру
-
     char cx = 2; // отступ до клеток по горищонтали
     char cy = 1; // отступ до клеток по вертикали
 
-    ss.frame = newwin(frameh, framew, fy, fx); // Окно с рамкой
-    box(ss.frame, 0, 0);                       // Рамка
-    keypad(ss.frame, TRUE);                    // Разрешаем ввод с клавиатуры
+    char framew = cw * BOARD_SIZE + cw_space * (BOARD_SIZE + 1) + 2; // ширина рамки
+    char frameh = ch * BOARD_SIZE + ch_space * (BOARD_SIZE + 1) + 2; // высота рамки
+
+    ss.frame = create_window(framew, frameh, "Игра");
 
     // Создаем дочерние окна для каждой клетки
     for (char i = 0; i < BOARD_CELLS; i++)
@@ -97,26 +108,23 @@ screen_state new_game_screen()
 
 void update_game_screen(screen_state *ss, game_state *gs)
 {
-    box(ss->frame, 0, 0);
-
     for (char i = 0; i < BOARD_CELLS; i++)
     {
         char row = ROW(i), col = COL(i);
-
-        WINDOW *win = ss->board[row][col];
+        WINDOW *w = ss->board[row][col];
 
         if (gs->board[row][col] > 0)
         {
-            box(win, 0, 0);
-            mvwprintw(win, 1, 2, "%2d", gs->board[row][col]);
+            box(w, 0, 0);
+            mvwprintw(w, 1, 2, "%2d", gs->board[row][col]);
         }
         else
         {
-            wclear(win);
+            wclear(w);
         }
+        wrefresh(w);
     }
     mvwprintw(ss->frame, getmaxy(ss->frame) - 1, (getmaxx(ss->frame) - 16) / 2, " номер хода: %d ", gs->moves);
-    wrefresh(ss->frame);
 }
 
 void clear_game_screen(screen_state *ss)
@@ -124,67 +132,39 @@ void clear_game_screen(screen_state *ss)
     for (char i = 0; i < BOARD_CELLS; i++)
     {
         char row = ROW(i), col = COL(i);
-        WINDOW *win = ss->board[row][col];
-        delwin(win);
+        WINDOW *w = ss->board[row][col];
+        close_window(w);
     }
-    wclear(ss->frame);   // Очищаем окно
-    wrefresh(ss->frame); // Обновляем экран
-    delwin(ss->frame);   // Удаляем окно
+    close_window(ss->frame);
 }
 
 void help()
 {
-    WINDOW *frame, *w;
+    char width = 50;  // Ширина окна с рамкой
+    char height = 12; // Высота окна с рамкой
 
-    char width = 50;                         // Ширина окна с рамкой
-    char height = 12;                        // Высота окна с рамкой
-    char x = (getmaxx(stdscr) - width) / 2;  // По центру
-    char y = (getmaxy(stdscr) - height) / 2; // По центру
-
-    frame = newwin(height, width, y, x);                 // Новое окно c рамкой
-    box(frame, 0, 0);                                    // Рамка у окна
-    mvwprintw(frame, 0, (width - 10) / 2, "┤ Помощь ├"); // Заголовок
-    wrefresh(frame);                                     // Отобразить окно
-
-    w = derwin(frame, height - 4, width - 8, 2, 4); // Новое окно с отступами для текста
+    WINDOW *frame = create_window(width, height, "Помощь");
+    WINDOW *w = derwin(frame, height - 4, width - 8, 2, 4); // Новое окно с отступами для текста
 
     wprintw(w, "Управление:\n\n");
     wprintw(w, "← ↑ ↓ → - перемещение фишек\n");
     wprintw(w, "      N - начать игру заново\n");
     wprintw(w, "      Q - закончить игру и выйти в меню\n");
     wprintw(w, "\n\nНажмите любую клавишу для возврата в меню");
-
     wrefresh(w);
 
     wgetch(frame); // Ждем нажатия на любую клавишу
 
-    delwin(w);       // Удаляем окно с текстом
-    wclear(frame);   // Очищаем окно с рамкой
-    wrefresh(frame); // Обновляем экран
-    delwin(frame);   // Удаляем окно
+    close_window(w);
+    close_window(frame);
 }
 
 void win()
 {
-    WINDOW *frame;
-
-    char width = 50;                         // Ширина окна с рамкой
-    char height = 5;                         // Высота окна с рамкой
-    char x = (getmaxx(stdscr) - width) / 2;  // По центру
-    char y = (getmaxy(stdscr) - height) / 2; // По центру
-
-    frame = newwin(height, width, y, x);                    // Новое окно c рамкой
-    box(frame, 0, 0);                                       // Рамка у окна
-    mvwprintw(frame, 0, (width - 13) / 2, " Вы выиграли "); // Заголовок
-    wrefresh(frame);                                        // Отобразить окно
-
-    mvwprintw(frame, 2, 4, "Нажмите любую клавишу для возврата в меню");
-
-    wgetch(frame); // Ждем нажатия на любую клавишу
-
-    wclear(frame);   // Очищаем окно с рамкой
-    wrefresh(frame); // Обновляем экран
-    delwin(frame);   // Удаляем окно
+    WINDOW *w = create_window(50, 5, "Вы выиграли");
+    mvwprintw(w, 2, 4, "Нажмите любую клавишу для возврата в меню");
+    wgetch(w);
+    close_window(w);
 }
 
 void play()
